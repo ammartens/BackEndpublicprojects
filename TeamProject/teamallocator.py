f399ebc9976118aff828199f8e6848d51b2360d6
ammartens <arianamartens@gmail.com>
2022-04-15 10:57:48 -0700

    bugs fixed

--- TeamProject/teamallocator.py
+++ TeamProject/teamallocator.py
@@ -2,7 +2,8 @@ import random
 players = ["Andrea", "Colton", "Ariana", "Kayla", "Anthony", "Destiny", "Jeanette",
 "Jacque", "Kody", "Kaden", "Kason", "Ashley", "Oswald", "Clancy", "Gilbert", "Marvie", "Alan", "Craig",
 "Lexi", "Jack"]
-print("Welcome to Team Allocator!!")
+
+print("Welcome to Dodgeball!!")
 
 while True:
     random.shuffle(players)
@@ -10,25 +11,22 @@ while True:
         \nType team or individual: ")
     if response == "team":
         team1 = players[:len(players)//2]
+        print("\nTeam 1 captain: " + random.choice(team1))
+        print("Team 1:")
         for player in team1:
             print(player)
         team2 = players[len(players)//2:]
-        for player in team2:
-            print(player)
-        print("\nTeam 1 captain: " + random.choice(team1))
-        print("Team 1:")
         print("\nTeam 2 captain: " + random.choice(team2))
         print("Team 2:")
-    for player in team1:
-        print(player)
-    for player in team2:
-        print(player)
-    response = input("Pick teams again? Type y or n: ")
-    if response == "n" :
-        break
+        for player in team2:
+            print(player)
     else: 
         for i in range(0,20,2):
             print(players[i] + " vs " + players[i+1])
             start = random.randrange(i, i+2)
-            print(players[start] + " \nstarts")
+            print(players[start] + " starts!\n")
+    response = input("Pick teams again? Type y or n: ")
+    if response == "n" :
+        break
+   
             
